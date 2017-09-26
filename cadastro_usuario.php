3595d44b5203b9868e9046db001249f4532d89ed
Lucas Delboni <lucas_cd460@hotmail.com>
2017-09-26 00:25:36 +0000

    feito tela de cadastro do usuario

---
+++ cadastro_usuario.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <?php include('includes.php'); ?>
+    </hed>
+    <body>
+        <div class="col-md-2 col-md-offset-5">
+            <h3>Cadastrar usuário</h3>
+            <form action="/index.php" method="post" target="_blank">
+                <div class="form-group">
+                    <label>Nome completo: </label><input type="text" class="form-control" name="nome"><br>
+                    <label>Email: </label><input type="text" class="form-control" name="email"><br>
+                    <label>Senha: </label><input type="password" class="form-control" name="senha"><br>
+                </div>
+                <input type="submit" class="btn btn-primary" value="Cadastrar">
+            </form>
+        </div>
+    </body>
+</html>
